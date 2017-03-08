9d34dcd62aa109ec050cb0f4aeac9df46766dae1
ahan <alvinhan1999@gmail.com>
2017-03-08 10:47:09 -0500

    initial layout

--- src/BinaryTree/MyBinaryTree.java
+++ src/BinaryTree/MyBinaryTree.java
@@ -2,4 +2,27 @@ package BinaryTree;
 
 public class MyBinaryTree {
 	
+	StudentInfo root;
+	int numItems = 0;
+	
+	public StudentInfo getRoot() {
+		return root;
+	}
+	
+	public void addToTree() {
+		
+	}
+	
+	public void traverseInorder() {
+		
+	}
+	
+	public void traversePreorder() {
+		
+	}
+
+	public void traversePostorder() {
+	
+	}
+
 }
--- src/BinaryTree/StudentInfo.java
+++ src/BinaryTree/StudentInfo.java
@@ -1,6 +1,12 @@
 package BinaryTree;
 
-
 public class StudentInfo {
-
+	
+	StudentInfo left;
+	StudentInfo right;
+	
+	int studentNumber;
+	String FirstName;
+	String LastName;
+	
 }
